0cd6ed77152d30acdb20eee650b67132d75363fb
MattReal <86115219+arielmatius@users.noreply.github.com>
2022-06-04 18:24:34 +0700

    pembuatan halaman homepage

---
+++ application/controllers/homepage.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class homepage extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('homepage_view');
+	}
+}
