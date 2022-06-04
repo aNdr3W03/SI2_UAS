c0a4185db4b25c9fac034dc7ba96e68a172c8c6e
arielmatius <arielmatius21@gmail.com>
2022-06-04 18:33:57 +0700

    penambahan homepage dan testing header

--- application/controllers/homepage.php
+++ application/controllers/homepage.php
@@ -6,6 +6,8 @@ class homepage extends CI_Controller
 
 	public function index()
 	{
+		$this->load->view('template/header');
 		$this->load->view('homepage_view');
+		$this->load->view('template/footer');
 	}
 }
---
+++ application/views/template/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.boots trap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font- awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <header>
+        <nav class="sb-topnav navbar navbar-expand navbar- dark bg-dark">
+            <a class="navbar-brand" href="index.html">SISTEM INFORMASI IKLC</a>
+            <<!-- Navbar Search-->
+                <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+                    <div class="input-group">
+                        <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria- describedby="basic-addon2" />
+                        <div class="input-group-append">
+                            <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                        </div>
+                    </div>
+                </form>
+                <!-- Navbar-->
+                <ul class="navbar-nav ml-auto ml-md-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data- toggle="dropdown" aria-haspopup="true" aria- expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                        <div class="dropdown-menu dropdown-menu- right" aria-labelledby="userDropdown">
+                            <a class="dropdown-item" href="#">Settings</a><a class="dropdown-item" href="#">Activity Log</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="login.html">Logout</a>
+                        </div>
+                    </li>
+                </ul>
+        </nav>
+    </header>}
