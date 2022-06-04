8d0096a65ac5be0a5551c1969c197465161f9927
arielmatius <arielmatius21@gmail.com>
2022-06-04 18:27:34 +0700

    .

--- application/controllers/homepage.php
+++ application/controllers/homepage.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class homepage extends CI_Controller {
+class homepage extends CI_Controller
+{
 
 	public function index()
 	{
