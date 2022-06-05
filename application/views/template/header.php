214b692b6e658098930f788e18f0d8554df9210a
arielmatius <arielmatius21@gmail.com>
2022-06-05 21:46:32 +0700

    mengubah font header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -13,6 +13,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
     <link rel="icon" href="assets/img/books.png" type="image/x-icon">
+
+    <!-- Link Font -->
+    <!-- FONTS -->
+    <!-- MANJARI 100 400 700-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manjari:wght@100;400;700&display=swap" rel="stylesheet">
+
+    <style>
+        div {
+            font-family: 'Manjari', sans-serif;
+        }
+    </style>
+    <!-- Link Font -->
+
     <title>Book</title>
 </head>
 
