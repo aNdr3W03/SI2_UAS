b553a210e2257195c8de049dd50e3efc34d99dd4
Al Imamul Luthfi <86113150+OxygenAddicted@users.noreply.github.com>
2022-06-05 23:49:13 +0700

    Update about_view.php

--- application/views/about_view.php
+++ application/views/about_view.php
@@ -1,11 +1,14 @@
 <head>
 
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
 
-    <div style="margin-top: 120px; text-align: center;">
+    <div style="margin-top: 130px; text-align: center;">
         <img src="assets\img\about_title.png" width="170px">
     </div>
 
@@ -13,7 +16,7 @@
         <img src="assets\img\about_pics.png" alt="pics" width="1200px">
     </div>
 
-    <div style="margin-top: 30px; text-align: center;">
+    <div style="margin-top: 50px; text-align: center;">
         <h1 style=" margin-font-weight: bold;">
             Membaca dapat membuka wawasan <br>
             dan tugas kami adalah membantu Anda
@@ -22,12 +25,97 @@
 
     <div style="margin-top: 10px; text-align: center; margin-left: 350px; margin-right: 350px">
         <p>
-            Fokus kami adalah membuat Buku Hehe menjadi sebuah platform membaca yang semakin mudah
-            digunakan oleh semua orang. Sejak 2022, kami sudah membantu 4 orang dari seluruh penjuru
+            Fokus kami adalah membuat Buku Hehe menjadi sebuah platform membaca yang semakin mudah 
+            digunakan oleh semua orang. Sejak 2022, kami sudah membantu 4 orang dari seluruh penjuru 
             dunia membaca 4 literatur.
         </p>
     </div>
 
-    <div style="margin-bottom: 100px;" />
+    <div style="text-align: center; margin-top: 50px;">
+        <img src="assets\img\about_pics2.png" alt="pics" width="1200px">
+    </div>
 
+    <div style="margin-top: 50px; text-align: center;">
+        <h1 style=" margin-font-weight: bold;">
+            Kami adalah tim kecil <br>
+            namun dengan tujuan yang besar
+        </h1>
+    </div>
+
+    <div style="margin-top: 10px; text-align: center; margin-left: 350px; margin-right: 350px">
+        <p>
+            Kami 100% independen dan berada di sini siap melayani kebutuhan membaca banyak orang
+            dan membantu mereka memperluas pandangan mereka terhadap apa yang mereka sukai.
+        </p>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-4 g-4" style="margin-top: 10px; text-align: center; margin-left: 150px; margin-right: 150px">
+        <div class="col">
+            <div class="card h-100">
+                <img src="assets\img\imamul_pic.jpeg" class="card-img-top" alt="member_pics">
+                <div class="card-body">
+                    <h4 class="card-title"> Al Imamul Luthfi </h4>
+                    <p class="card-title"> 201401032 </p>
+                    <p class="card-text">Pimpinan Human Resource Department dari Buku Hehe. <br> Medan, Indonesia</p>
+                    <a href="https://www.github.com/OxygenAddicted" style="margin-right: 20px; color: transparent;">
+                        <input type="image" name="GitHub" src="assets\img\github_logo.png" style="width: 40px;" alt="text"> 
+                    </a>
+                    <a href="https://www.linkedin.com/in/al-imamul-luthfi-9a86561a5/">
+                        <input type="image" name="LinkedIn" src="assets\img\linkedin_logo.png" style="width: 40px;" alt="text"> 
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="assets\img\andrew_pic.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Andrew Benedictus Jamesie</h5>
+                    <p class="card-title"> 201401035 </p>
+                    <p class="card-text">Chief Executive Officer dari Buku Hehe. <br> Medan, Indonesia</p>
+                    <a href="https://github.com/aNdr3W03" style="margin-right: 20px; color: transparent;">
+                        <input type="image" name="GitHub" src="assets\img\github_logo.png" style="width: 40px;" alt="text"> 
+                    </a>
+                    <a href="https://www.linkedin.com/in/andrew-benedictus-jamesie-07658a1b3/">
+                        <input type="image" name="LinkedIn" src="assets\img\linkedin_logo.png" style="width: 40px;" alt="text"> 
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+            <img src="assets\img\ariel_pic.jpeg" class="card-img-top" alt="member_pics">
+                <div class="card-body">
+                    <h5 class="card-title"> Ariel Matius Surbakti </h5>
+                    <p class="card-title"> 201401038 </p>
+                    <p class="card-text">Ketua Divisi Back-End Web Programmming dari Buku Hehe. <br> Medan, Indonesia</p>
+                    <a href="https://github.com/arielmatius" style="margin-right: 20px; color: transparent;">
+                        <input type="image" name="GitHub" src="assets\img\github_logo.png" style="width: 40px;" alt="text"> 
+                    </a>
+                    <a href="https://www.linkedin.com/in/ariel-matius-surbakti-a96a18227/">
+                        <input type="image" name="LinkedIn" src="assets\img\linkedin_logo.png" style="width: 40px;" alt="text"> 
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+            <img src="assets\img\avin_pic.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> Avin Chaili Salim </h5>
+                    <p class="card-title"> 201401040 </p>
+                    <p class="card-text">Ketua Divisi Front-End Web Programming dari Buku Hehe. <br> Medan, Indonesia</p>
+                    <a href="https://github.com/AvinChailiSalim" style="margin-right: 20px; color: transparent;">
+                        <input type="image" name="GitHub" src="assets\img\github_logo.png" style="width: 40px;" alt="text"> 
+                    </a>
+                    <a href="https://www.linkedin.com/in/avin-salim-682218201/">
+                        <input type="image" name="LinkedIn" src="assets\img\linkedin_logo.png" style="width: 40px;" alt="text"> 
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div style="margin-bottom: 100px;"/>
+    
 </body>
