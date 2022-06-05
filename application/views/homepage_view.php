ffe3dd87e768d9a74ef2299126320902ab5d3c18
arielmatius <arielmatius21@gmail.com>
2022-06-05 23:07:13 +0700

    update halaman utama

--- application/views/homepage_view.php
+++ application/views/homepage_view.php
@@ -4,44 +4,67 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <style>
+        .carousel-caption {
+            background-color: white;
+            border-radius: 7px;
+        }
+    </style>
 </head>
 
 <body>
+
+    <div style="margin-top: 70px; text-align: center;">
+        <h1 style=" margin-font-weight: bold;">
+            <br>
+            MARI MEMBACA!!
+        </h1>
+    </div>
+
     <!-- Carousel -->
-    <div style="margin: auto;width:50%;margin-top: 100px" id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div style="margin:auto;width:50%;" id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 4"></button>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://www.midlothianlibrary.org/sitemedia/images/BooksandMedia/bookreview2.jpg" class="d-block w-100" alt=".">
+        <div class="carousel-inner" style="border-radius:10px">
+            <div class="carousel-item active" data-bs-interval="10000">
+                <img src="https://wallpapercave.com/wp/wp2036901.jpg" class="d-block w-100" alt="assets\img\book_carousel-1.png">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
+                    <h5>Al Imamul Luthfi</h5>
+                    <p>Tetaplah santai walau deadline mengintai</p>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="https://wallpapercave.com/wp/wp2046075.jpg" class="d-block w-100" alt="assets\img\book_carousel-2.png">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Andrew Benedictus Jamesie</h5>
+                    <p>Jika Anda paham, tetaplah mengejar SCL. Jika tidak, tetaplah berpura-pura paham</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                <img src="https://wallpapercave.com/wp/wp6651848.jpg" class="d-block w-100" alt="assets\img\book_carousel-3.png">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
+                    <h5>Ariel Matius Surbakti</h5>
+                    <p>Jika orang lain bisa, kenapa harus kita?</p>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                <img src="https://wallpapercave.com/wp/wp6651900.jpg" class="d-block w-100" alt="assets\img\book_carousel-4.png">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
+                    <h5>Avin Chaili Salim</h5>
+                    <p>Jika orang lain bisa, kenapa harus kita?</p>
                 </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
