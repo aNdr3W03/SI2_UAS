b26219e7a915f5c23173b7902cedc9db68dc7c95
arielmatius <arielmatius21@gmail.com>
2022-06-10 23:37:32 +0700

    penambahan fitur validasi dalam penambahan buku baru

--- application/controllers/buku.php
+++ application/controllers/buku.php
@@ -1,16 +1,16 @@
 <?php
-class Book extends CI_Controller
+class Buku extends CI_Controller
 {
     public function index()
     {
         $this->load->view('template/header');
-        $this->load->model('book_model');
+        $this->load->model('buku_model');
 
-        $data = $this->book_model->GetBuku('tbl_buku');
+        $data = $this->buku_model->GetBuku('tbl_buku');
 
         $data = array('data' => $data);
 
-        $this->load->view('book_view', $data);
+        $this->load->view('buku_view', $data);
         $this->load->view('template/footer');
     }
 }
