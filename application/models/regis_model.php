98d6896af34d0e59b2889d1baa521f1f98d66684
arielmatius <arielmatius21@gmail.com>
2022-06-11 14:20:16 +0700

    menambahkan fitur register admin

---
+++ application/models/regis_model.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowe d');
+class Regis_model extends CI_Model
+{
+    public function construct()
+
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function GetBuku($table)
+    {
+        $res = $this->db->get($table); // Kode ini berfungsi untuk memilih tabel yang akan ditampilkan
+        return $res->result_array(); // Kode ini digunakan untuk mengembalikan hasil operasi $res menjadi sebuah array
+    }
+
+    public function GetWhere($table, $data)
+    {
+        $res = $this->db->get_where($table, $data);
+        return $res->result_array();
+    }
+
+    public function Insert($table, $data)
+    {
+        $res = $this->db->insert($table, $data); // Kode ini digunakan untuk memasukan record baru kedalam sebuah tabel
+        return $res; // Kode ini digunakan untuk mengembali kan hasil $res
+    }
+
+    public function Update($table, $data, $where)
+    {
+        $res = $this->db->update($table, $data, $where); // Kode ini digunakan untuk merubah record yang sudah ada dalam sebuah tabel
+        return $res;
+    }
+
+    public function Delete($table, $where)
+    {
+        $res = $this->db->delete($table, $where); // Kode ini digunakan untuk menghapus record yang sudah ada
+        return $res;
+    }
+}
+?>
---
+++ application/views/regisAdmin_view.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register Admin - Buku HEHE</title>
+</head>
+
+<body>
+    <table>
+        <?php echo validation_errors(); ?>
+        <?php echo form_open('regisAdmin/insert'); ?>
+        <form method="post" action="<?php echo base_url('regisAdmin/insert'); ?>">
+            PENAMBAHAN ADMIN
+            <tr>
+                <td>ID</td>
+                <td>:</td>
+                <td><input type="text" name="id" placeholder="ID Admin"></td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><input type="text" name="username" value="" <?php echo set_value('username'); ?> placeholder="Nama Admin"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>:</td>
+                <td><input type="text" name="password" value="" <?php echo set_value('password'); ?> placeholder="Password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><input type="submit" name="submit" value="Simpan"></td>
+            </tr>
+        </form>
+    </table>
+</body>
+
+</html>
