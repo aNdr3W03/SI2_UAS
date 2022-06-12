08b2530e53ee1b143d78a34ee49bd975ceafc0fc
arielmatius <arielmatius21@gmail.com>
2022-06-12 14:28:31 +0700

    update regisAdmin

--- application/controllers/regisAdmin.php
+++ application/controllers/regisAdmin.php
@@ -7,7 +7,7 @@ class Regisadmin extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('login_model');
+        $this->load->model('regis_model');
     }
 
     function index()
@@ -18,9 +18,9 @@ class Regisadmin extends CI_Controller
     public function insert()
     {
         $this->load->model('regis_model');
-        // Cek apakah Nama Buku kosong
+        // Cek apakah Nama Admin kosong
         $this->form_validation->set_rules('username', 'Nama', 'required', array('required' => 'Nama Admin harus diisi'));
-        // Cek apakah Penulis Buku kosong
+        // Cek apakah Password kosong
         $this->form_validation->set_rules('password', 'Password', 'required', array('required' => 'Password harus diisi'));
 
         if ($this->form_validation->run() == TRUE) {
@@ -37,59 +37,59 @@ class Regisadmin extends CI_Controller
         }
     }
 
-    public function delete_data($id_buku)
+    public function delete_data($id)
     {
-        $id_buku = array('id_buku' => $id_buku);
-        $this->load->model('buku_model');
-        $this->buku_model->Delete('tbl_buku', $id_buku);
+        $id = array('id' => $id);
+        $this->load->model('regis_model');
+        $this->regis_model->Delete('tbl_admin', $id);
         redirect(base_url('admin'), 'refresh');
     }
 
-    public function edit_data($id_buku)
+    public function edit_data($id)
     {
-        $this->load->model('buku_model');
-        $siswa = $this->buku_model->GetWhere('tbl_buku', array('id_buku' => $id_buku));
+        $this->load->model('regis_model');
+        $siswa = $this->regis_model->GetWhere('tbl_admin', array('id' => $id));
         $data = array(
-            'id_buku' => $siswa[0]['id_buku'],
+            'id' => $siswa[0]['id'],
             'username' => $siswa[0]['username'],
             'password' => $siswa[0]['password'],
         );
-        $this->load->view('editBuku_view', $data);
+        $this->load->view('editAdmin_view', $data);
     }
 
     public function update_data()
     {
-        $id_buku = $_POST['id_buku'];
+        $id = $_POST['id'];
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        // Cek apakah nama kosong
-        $this->form_validation->set_rules('username', 'Nama', 'required', array('required' => 'Nama Buku harus diisi'));
-        // Cek apakah KOM kosong
-        $this->form_validation->set_rules('password', 'Penulis', 'required', array('required' => 'Penulis Buku harus diisi'));
+        // Cek apakah Nama kosong
+        $this->form_validation->set_rules('username', 'Nama', 'required', array('required' => 'Nama Admin harus diisi'));
+        // Cek apakah Password kosong
+        $this->form_validation->set_rules('password', 'Password', 'required', array('required' => 'Password harus diisi'));
 
         if ($this->form_validation->run() == TRUE) {
             $data = array(
-                'id_buku' => $id_buku,
+                'id' => $id,
                 'username' => $username,
                 'password' => $password
             );
 
-            $where = array('id_buku' => $id_buku);
-            $this->load->model('buku_model');
-            $res = $this->buku_model->Update('tbl_buku', $data, $where);
+            $where = array('id' => $id);
+            $this->load->model('regis_model');
+            $res = $this->regis_model->Update('tbl_admin', $data, $where);
             if ($res > 0) {
                 redirect(base_url('admin'), 'refresh');
             }
         } else {
-            $this->load->model('buku_model');
-            $siswa = $this->buku_model->GetWhere('tbl_buku', array('id_buku' => $id_buku));
+            $this->load->model('regis_model');
+            $siswa = $this->regis_model->GetWhere('tbl_admin', array('id' => $id));
             $data = array(
-                'id_buku' => $siswa[0]['id_buku'],
+                'id' => $siswa[0]['id'],
                 'username' => $siswa[0]['username'],
                 'password' => $siswa[0]['password'],
             );
-            $this->load->view('editBuku_view', $data);
+            $this->load->view('editAdmin_view', $data);
         }
     }
 }
